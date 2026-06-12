f36459efada1964ed1cdea498086891e47679f6f
impact-jap <43191369+impact-jap@users.noreply.github.com>
2026-06-12 17:14:15 -0300

    Add files via upload

---
+++ pkg/synclyrics_wasm_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_synclyricsruntime_free: (a: number, b: number) => void;
+export const convert_text: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => [number, number, number, number];
+export const inspect_text: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const synclyricsruntime_document_json: (a: number) => [number, number, number, number];
+export const synclyricsruntime_new: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number, number];
+export const synclyricsruntime_render_nodes_json: (a: number) => [number, number, number, number];
+export const synclyricsruntime_seek_time_for_node: (a: number, b: number, c: number) => [number, number];
+export const synclyricsruntime_state_at: (a: number, b: number) => [number, number, number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
